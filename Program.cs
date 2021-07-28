f2cdb928793d16c2f67202e613d4957ccffce7af
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-28 09:37:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Composition
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbMIgrator = new DbMigrator(new Logger());
+
+            var installer = new Installer(new Logger());
+
+            dbMIgrator.Migrate();
+            installer.Install();
+
+        }
+    }
+}
